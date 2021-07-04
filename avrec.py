c6b3840cf6d518d6e891a425f544ae68ad3c027c
Adam Bocco <adam.bocco@gmail.com>
2021-07-04 08:58:39 -0400

    updates

--- avrec.py
+++ avrec.py
@@ -342,7 +342,7 @@ def makeLineBreaks(stringToBreak, breakIndex):
     
 
 if __name__ == "__main__":
-    time.sleep(6)
+    time.sleep(10)
     pba = Application()
     pba.root.mainloop()
     exit()
